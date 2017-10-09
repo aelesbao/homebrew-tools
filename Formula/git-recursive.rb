aabdfa5166cf50a364afa75052391ce55da2f438
Augusto Elesbão <aelesbao@gmail.com>
2017-10-09 23:04:40 +0200

    bump git-recursive 0.1.2

--- Formula/git-recursive.rb
+++ Formula/git-recursive.rb
@@ -1,17 +1,20 @@
 class GitRecursive < Formula
   desc "Execute git operations in multiple repositories"
   homepage "https://github.com/aelesbao/git-recursive"
-  url "https://github.com/aelesbao/git-recursive/archive/0.1.1.tar.gz"
-  sha256 "be8e8e557639a0534edffdb875b3587e9d546267b0eea5432055af574e4a999e"
+  url "https://github.com/aelesbao/git-recursive/archive/0.1.2.tar.gz"
+  sha256 "ca18bb20393805a7f67e988e43d02fb4caf2e9119910d2abba0ce46a33fa6d78"
+
+  head "https://github.com/aelesbao/git-recursive.git", :branch => "master"
+
+  bottle :unneeded
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
-    system "git", "init"
-    system "git", "recursive", "add", "test", testpath
-    system "git", "recursive", "list"
-    system "git", "recursive", "rm", "test"
+    system bin/"git-recursive", "add", "test", testpath
+    assert_match(/^test.+#{testpath}/, shell_output("#{bin}/git-recursive list"))
+    system bin/"git-recursive", "rm", "test"
   end
 end
