e289dc3d4e15d51ad31e5e50f62544251ecb8e30
Augusto Elesbão <aelesbao@gmail.com>
2021-04-28 18:19:58 +0200

    bump git-recursive 0.2.1

--- Formula/git-recursive.rb
+++ Formula/git-recursive.rb
@@ -1,8 +1,8 @@
 class GitRecursive < Formula
   desc "Execute git operations in multiple repositories"
   homepage "https://github.com/aelesbao/git-recursive"
-  url "https://github.com/aelesbao/git-recursive/archive/0.2.0.tar.gz"
-  sha256 "0db2a9f8624303f1bab072084ba9b2916009263b9d0460e104bf4df54a3a46ad"
+  url "https://github.com/aelesbao/git-recursive/archive/0.2.1.tar.gz"
+  sha256 "494cc1fd1271cc8bf3e57b8d025d3dad06679924524f27ff2251c5f81d9a3669"
 
   head "https://github.com/aelesbao/git-recursive.git", :branch => "master"
 
